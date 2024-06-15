4249d23b3d5873e47c14448e62ff2e9bb87c666b
Mustafa Elsharawy <74271005+MeMoElprince@users.noreply.github.com>
2024-06-15 12:23:31 +0300

    Update Link to my resume

--- src/Components/Navbar/Items.jsx
+++ src/Components/Navbar/Items.jsx
@@ -33,7 +33,7 @@ const Items = ({className}) => {
                     </NavLink>
                 </li>
                 <li className="nav-item flex items-center gap-2 px-1 py-2">
-                    <NavLink to="https://drive.google.com/file/d/1-3yuIIkT6iTvx9yjDLYRz_tllsbVppT1/view" target='_blank'  className="nav-link flex gap-3"> 
+                    <NavLink to="https://docs.google.com/document/d/1R71Si3feOK-NjYo13-z9Sgi3onYtvuxyS_HDa8qi73Q/edit?usp=sharing" target='_blank'  className="nav-link flex gap-3"> 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
